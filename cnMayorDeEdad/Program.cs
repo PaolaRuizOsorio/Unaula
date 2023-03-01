d085b5c02aeabfb4e44d261bdf79fc0c4927616d
PaolaRuizOsorio <126583659+PaolaRuizOsorio@users.noreply.github.com>
2023-02-28 22:40:07 -0500

    Add files via upload

---
+++ cnMayorDeEdad/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnMayorDeEdad
+{
+    /// <summary>
+    /// Nombre: Paola Ruiz Osorio 
+    /// Fecha: 25/12/2023
+    /// Descripción: Validar si la persona es Mayor de edad y en caso de derlo pedir apellidos y correo electronico
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor escriba su primer nombre y segundo nombre en caso de tenerlo");
+            String Nombre =Console.ReadLine();
+            //Llamar metodo
+            ComprobarEdad(Nombre);
+            
+            Console.ReadKey();
+        }
+        //Metodo para validar la edad
+        public static void ComprobarEdad(String Nombre)
+        {
+            Console.WriteLine("Por favor ingrese su edad");
+            int Edad = Convert.ToInt16( Console.ReadLine());
+
+            //Condicional para comprobar si es mayor de edad y continuar el proceso 
+            if (Edad > 0 && Edad<=18)
+            {
+                Console.WriteLine("Hola " +Nombre+" eres menor de edad,Muchas gracias");
+            }
+            else if (Edad > 18)
+            {
+                Console.WriteLine("Hola " + Nombre + " ingresa tus apeliidos");
+                String Apellidos = Console.ReadLine();
+                Console.WriteLine("Ingresa tu correo");
+                String CorreoElectronico = Console.ReadLine();
+                Console.WriteLine(Nombre + " tus datos han sido agregados con exito");
+            }
+            else 
+            {
+                Console.WriteLine("Por favor ingrese una edad correcta");
+            }
+
+        }
+
+    }
+}
---
+++ cnTemperatura/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnTemperatura
+{
+    /// <summary>
+    /// Nombre:Paola Ruiz Osorio
+    /// Fecha: 25/02/2023
+    /// Descripción:Conversion de temperaturas entre Farenheit y Celsius
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor ingrese la temperatura");
+            double Temperatura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Marque el tipo de temperatura que ingreso");
+            Console.WriteLine("Para Celsius marque 1");
+            Console.WriteLine("Para Farenheit marque 2");
+            
+            int Dato = Convert.ToInt32(Console.ReadLine());
+            // Escoger el tipo de grados registrados, y en su debido caso hacer la conversion invocando los metodos
+            switch (Dato)
+            {
+                case 1:
+                    Console.WriteLine("La temperatura convertida a Farenheit es de : " + ValidarFarenheit(Temperatura)+" °");
+                        break;
+                case 2:
+                    Console.WriteLine("La temperatura convertida a Celsius es de : " + ValidarCelsius(Temperatura)+" °");
+                        break;
+            
+            }
+            Console.ReadKey();
+        }
+        //Metodo cambio de Celsius a Farenheit
+        static double ValidarCelsius(Double Temperatura)
+
+        {
+            Double Celsius;
+            Celsius = (Temperatura - 32) / 1.8;
+            return Celsius;
+        }
+        //Metodo cambio de Farenheit a Celsius 
+        static double ValidarFarenheit(Double Temperatura)
+        {
+            Double Farenheit;
+            Farenheit = (Temperatura * 1.8) + 32;
+            return Farenheit;
+        }
+    }
+}
